975e11d4f93f7c5b9a0aadf4e63c6b755cf385f7
Avyvka <lamonov.yura@gmail.com>
2023-03-04 17:48:00 +0300

    Small fixes

--- SortingAlgorithms/ArraySorter/BubbleSorter.hpp
+++ SortingAlgorithms/ArraySorter/BubbleSorter.hpp
@@ -5,24 +5,16 @@
 template <class T>
 class BubbleSorter : public AbstractArraySorter<T>
 {
-private:
-    void swap(T& arg0, T& arg1)
-    {
-        T temp = arg0;
-        arg0 = arg1;
-        arg1 = temp;
-    }
-
 public:
 	void sort(Array<T>& array) override
 	{
-        for (T step = 0; step < array.size() - 1; ++step)
+        for (std::size_t step = 0; step < array.size() - 1; step++)
         {
-            for (T index = 0; index < array.size() - step - 1; ++index)
+            for (std::size_t index = 0; index < array.size() - step - 1; index++)
             {
                 if (array[index] > array[index + 1])
                 {
-                    swap(array[index], array[index + 1]);
+                    array.swap(index, index + 1);
                 }
             }
         }
