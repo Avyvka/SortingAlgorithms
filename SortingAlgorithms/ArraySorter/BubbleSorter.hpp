38c61e6ff811f00ea62ce60799a5de26ab9aedf2
Avyvka <lamonov.yura@gmail.com>
2023-03-08 14:56:41 +0300

    perfomance

--- SortingAlgorithms/ArraySorter/BubbleSorter.hpp
+++ SortingAlgorithms/ArraySorter/BubbleSorter.hpp
@@ -15,12 +15,21 @@ public:
 	{
         for (std::size_t step = 0; step < array.size() - 1; step++)
         {
+            bool wasSwap = false;
+
             for (std::size_t index = 0; index < array.size() - step - 1; index++)
             {
                 if (array[index] > array[index + 1])
                 {
                     array.swap(index, index + 1);
+                    wasSwap = true;
                 }
+            }
+
+            // Eсли после выполнения внутреннего цикла не произошло ни одного обмена,
+            // то массив уже отсортирован, и продолжать что - то делать бессмысленно
+            if (!wasSwap) {
+                return;
             }
         }
 	}
